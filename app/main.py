575d83af2eb30a012754ad514c69c7b0b4c0c3d7
Robert Klajko <robert_klajko@epam.com>
2020-12-26 21:38:20 +0100

    Small update :-)

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Item(BaseModel):
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "admin")
-    correct_password = secrets.compare_digest(credentials.password, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "PolgarJeno")
     if not (correct_password and correct_username):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
